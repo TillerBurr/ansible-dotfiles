c2eba22a8b3cb7c97821d35b13068e72dc34d5c3
Tyler Baur <baur.tyler@gmail.com>
2025-09-29 09:47:58 -0500

    update

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -1,6 +1,19 @@
 return {
 	{
 		"stevearc/conform.nvim",
+          event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+        keys = {
+    {
+      -- Customize or remove this keymap to your liking
+      "<leader>f",
+      function()
+        require("conform").format({ async = true, lsp_fallback = true })
+      end,
+      mode = "",
+      desc = "Format buffer",
+    },
+  },
 		opts = {
 			formatters_by_ft = {
 				lua = { "stylua" },
